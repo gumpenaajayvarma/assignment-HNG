b1f8fde029612a37f2d81788764d61c30587020f
agumpena <ajay.gumpena.ext@nokia.com>
2020-12-22 17:11:34 +0530

    productsToolbar updated

--- src/components/ProductToolBar/index.js
+++ src/components/ProductToolBar/index.js
@@ -6,8 +6,8 @@ function ProductToolBar(props) {
     // we get sorts and filters as props.
     return <Col xs={12} style={{padding: '10px 0'}}>
         <p>{props.title} - {props.totalCount} products </p>
-        <button className='toolbar-btn' onClick={}>Sort</button>
-        <button className='toolbar-btn' onClick={}>filter</button>
+        <button className='toolbar-btn'>Sort</button>
+        <button className='toolbar-btn'>filter</button>
     </Col>
 }
 
